849ed1832e7a32552347b304bc3f8ba698a815eb
auz <lankyauz@gmail.com>
2022-07-25 14:06:31 -0600

    - The grid is no longer created, only populated, by javascript. - Players can now choose where to place their marks by clicking a space on the grid, and turns will alternate accordingly.

--- script.js
+++ script.js
@@ -1,35 +1,49 @@
-// Your main goal here is to have as little global code as possible. Try tucking everything away inside of a module or factory.
-// Rule of thumb: if you only ever need ONE of something (gameBoard, displayController), use a module.
-// If you need multiples of something (players!), create them with factories.
-
-const gridCont = document.querySelector('.gridCont');
+const cellArray = document.querySelectorAll('.gridCell');
 
 // game board object
 const gameBoard = (() => {
-    const gameState = ['x','o','x','o','x','o','x','o','x'];
+    const gameState = ['','','','','','','','','',];
+    const determinePlayer =()=>{
+        if(xPlayer.turnCount<=oPlayer.turnCount){
+            ++xPlayer.turnCount
+            return xPlayer.boardMark;
+        }
+        else if(oPlayer.turnCount<xPlayer.turnCount){
+            ++oPlayer.turnCount
+            return oPlayer.boardMark;
+        };
+    };
+    cellArray.forEach((x)=>{
+        x.addEventListener('click',()=>{
+            let cellNumber = x.getAttribute('data-cell-number') - 1;
+            if(gameState[cellNumber]!==''){
+                return;
+            };
+            gameState[cellNumber] = `${determinePlayer()}`;
+            console.log(xPlayer.turnCount, oPlayer.turnCount)
+            displayController.updateBoard(gameBoard.gameState)
+        });
+    });
     return {gameState,};
 })();
 
 // display controller object
 const displayController = (() => {
+    const cellContentArray = document.querySelectorAll('.gridCell p');
     const updateBoard = (x) => {
         for(let [i,index] = [x.length,0];i>0;--i,++index){
-            const gridCell = document.createElement('div');
-            gridCell.classList.add('gridCell');
-            gridCell.setAttribute('data-cell-number',`${index + 1}`); // *
-            const cellContent = document.createElement('p')
-            cellContent.textContent = `${x[index]}`;
-            gridCell.appendChild(cellContent);
-            gridCont.appendChild(gridCell);
-        }
+            cellContentArray[index].textContent = `${x[index]}`;
+        };
     }
     return{updateBoard,};
 })();
 
-
 // factory to create players
-const PlayerFactory = () => {
-    
+const PlayerFactory = (boardMark) => {
+    let turnCount = 0;
+    return {boardMark,turnCount};
 };
+const xPlayer = PlayerFactory('x');
+const oPlayer = PlayerFactory('o');
+
 displayController.updateBoard(gameBoard.gameState);
-console.log();
